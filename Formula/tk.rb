b841bb728035946008d3ef7bda61ac57832514de
Greg Wedow <gregwedow@gmail.com>
2026-01-02 11:56:10 -0500

    init: add tk formula

---
+++ Formula/tk.rb
@@ -0,0 +1,15 @@
+class Tk < Formula
+  desc "Minimal ticket tracking in bash"
+  homepage "https://github.com/wedow/ticket"
+  url "https://github.com/wedow/ticket/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "UPDATE_ON_RELEASE"
+  license "MIT"
+
+  def install
+    bin.install "ticket" => "tk"
+  end
+
+  test do
+    system "#{bin}/tk", "help"
+  end
+end
---
+++ README.md
@@ -0,0 +1,14 @@
+# homebrew-tap
+
+Personal Homebrew formulas.
+
+## Install
+
+```bash
+brew tap wedow/tap
+brew install tk
+```
+
+## Formulas
+
+- **tk** - Minimal ticket tracking in bash
